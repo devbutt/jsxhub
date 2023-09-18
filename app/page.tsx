e8ffd11d43612214ea952981a38ac905013d80a0
devbutt <oneill.tony@outlook.com>
2023-09-18 12:40:33 +0100

    added import for path

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import { Inter } from "next/font/google";
 
+import path from "path";
+path.resolve("./next.config.cjs");
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
